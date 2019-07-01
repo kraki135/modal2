215cca1bd8f11ef837a81303cf9c6957f6a64f7c
kraki135 <kraki135@gmail.com>
2019-07-01 07:49:35 +0200

    z domu modal2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var btn_edit = document.querySelector('.btn-edit');
 var btn_cancel = document.querySelector('.btn-cancel')
 var btn_confirm = document.querySelector('.btn-confirm');
-var modal = document.querySelector('.modal');
-var backdrop = document.querySelector('.backdrop');
+var modal;
+var backdrop;
 var outputParagraf = document.querySelector('#quote');
 var textEdit = document.querySelector('.modal textarea');
 var quote = 'To jest test modal. Zmień w nim tekst i dodaj coś od siebie.';
@@ -13,38 +13,30 @@ function updateParagraph() {
 }
 
 function closeModal() {
-  modal.style.display = 'none';
-    backdrop.style.display = 'none';
+  
 }
 
 updateParagraph();
 btn_edit.addEventListener('click', function() {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    backdrop = document.createElement('div');
+    backdrop.classList.add('backdrop');
+    backdrop.addEventListener('click', closeModal);
     editedQuote = quote;
-    textEdit.value = quote;
-    
-
+    textEdit.value = quote; 
 });
+//4:43
+/**
+ * btn_cancel.addEventListener('click', closeModal);
 
-btn_cancel.addEventListener('click', closeModal);
-backdrop.addEventListener('click', closeModal);
 
 btn_confirm.addEventListener('click', function() {
-    
+     closeModal();
      if(editedQuote.trim().length > 0) {
       quote = editedQuote;
       updateParagraph();
-     }else {
-
      }
-     
-
-
-
-    closeModal();
-    
 });
+ */
 textEdit.addEventListener('input', function() {
       editedQuote = textEdit.value;
       
